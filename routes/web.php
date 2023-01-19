263eab55f1ecb064483df32f3850b127ea65ac45
adrianglez16 <adriannglez6@gmail.com>
2023-01-19 19:33:02 +0000

    hecho hasta el ej de las fechas

--- routes/web.php
+++ routes/web.php
@@ -33,5 +33,54 @@ Route::middleware('auth')->group(function () {
 Route::get('community', [App\Http\Controllers\CommunityLinkController::class, 'index']);
 Route::post('community', [App\Http\Controllers\CommunityLinkController::class, 'store']);
 
+// 1) Crea una ruta que tenga un parámetro que sea opcional y comprueba que funciona
+Route::get('/community/{nombre?}', function ($nombre) {
+    return "Mi nombre es $nombre";
+});
+
+// Crea una ruta que tenga un parámetro que sea opcional y tenga un valor por defecto en caso de que no se especifique.
+Route::get('/ruta/{nombre?}', function ($nombre = "No has introducido ningún nombre") {
+    return $nombre;
+});
+
+// Crea una ruta que atienda por POST y compruébala con Postman.
+Route::post('/rutilla', function () {
+    $name = $_POST['name'];
+    return $name;
+});
+
+//Crea una ruta que atienda por GET y por POST (en un único método) y compruébalas.
+
+Route::match(['get', 'post'], '/rutikele', function ($apellido = "glez") {
+    return $apellido;
+
+});
+
+// Crea una ruta que compruebe que un parámetro está formado sólo por números.
+
+
+
+//Crea una ruta con dos parámetros que compruebe que el primero está formado sólo por letras y el segundo sólo por números.
+
+// 2) Utiliza el helper env para que cuando se acceda a la ruta /host nos devuelva la dirección IP donde se encuentra la base de datos de nuestro proyecto.
+Route::get('/host', function () {
+    $host = env('DB_HOST');
+
+    return $host;
+});
+
+// Utiliza el helper config para que cuando se acceda a la ruta /timezone se muestre la zona horaria.
+// Está dentro del archivo app.php en la línea 72
+
+Route::get('/timezone', function(){
+    $time = config('app.timezone');
+
+    return $time;
+});
+
+Route::view('/inicio', 'home');
+
+Route::view('/fecha,', 'fecha', ['dia' => date('d'), 'mes' => date('m'), 'year' => date('Y')]); 
+
 
 require __DIR__.'/auth.php';
